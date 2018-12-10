97ef34dd14ced266ef0c2b5f59a9d0f5d94dfd99
Mattia Durli <mdurli@gmail.com>
2018-12-10 15:06:22 +0100

    AppInsight

--- XamAppGitHub/XamAppGitHub.Android/MainActivity.cs
+++ XamAppGitHub/XamAppGitHub.Android/MainActivity.cs
@@ -7,6 +7,10 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
 namespace XamAppGitHub.Droid
 {
     [Activity(Label = "XamAppGitHub", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
@@ -16,6 +20,8 @@ namespace XamAppGitHub.Droid
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
+
+            AppCenter.Start("f07f5a74-5747-4d3f-950e-4aa03ab37f10", typeof(Analytics), typeof(Crashes));
 
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
